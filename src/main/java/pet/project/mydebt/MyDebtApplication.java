bf480b9069e03ce1bd9b939c366869ed5dab24d4
A-leksand-r <dykan.prod@yandex.ru>
2024-02-20 14:05:55 +0300

    Создание проекта

---
+++ src/main/java/pet/project/mydebt/MyDebtApplication.java
@@ -0,0 +1,13 @@
+package pet.project.mydebt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDebtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDebtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pet/project/mydebt/MyDebtApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.project.mydebt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDebtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
